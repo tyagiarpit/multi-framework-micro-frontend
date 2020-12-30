140739150d811670276ce99dc81a841b4102b296
Manfred Steyer <manfred.steyer@gmx.net>
2020-12-30 17:03:44 +0100

    add shell to lerna.json

--- mfe4-react/app.js
+++ mfe4-react/app.js
@@ -1,22 +1,77 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 
 class App extends React.Component {
 
   render() {
     const reactVersion = require('./package.json').dependencies['react'];
+      return (
+          <Router>
+              <h1>
+                  Micro Frontend 4
+                  <img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" height="30"></img>
+                  </h1>
+              <p>
+                React Version: {reactVersion}
+              </p>
+              <div>
 
-    return ([
-        <h1>
-          Micro Frontend 4
-          <img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" height="30"></img>
-        </h1>,
-        <p>
-          React Version: {reactVersion}
-        </p>
-    ])
+                  <Link to="/mfe4/">Home</Link>
+                  <br />
+                  <Link to="/mfe4/a">Route A</Link>
+                  <br />
+                  <Link to="/mfe4/b">Route B</Link>
+
+                  <hr />
+
+                  {}
+                  <Switch>
+                      <Route exact path="/mfe4/">
+                          <Home />
+                      </Route>
+                      <Route path="/mfe4/a">
+                          <RouteA />
+                      </Route>
+                      <Route path="/mfe4/b">
+                          <RouteB />
+                      </Route>
+                  </Switch>
+              </div>
+          </Router>
+      );
   }
 }
+
+function Home() {
+    return (
+        <div>
+            <h2>Home</h2>
+        </div>
+    );
+}
+
+function RouteA() {
+    return (
+        <div>
+            <h2>Route A</h2>
+        </div>
+    );
+}
+
+function RouteB() {
+    return (
+        <div>
+            <h2>Route B</h2>
+        </div>
+    );
+}
+
 
 class Mfe4Element extends HTMLElement {
   connectedCallback() {
--- shell/src/app/decl.d.ts
+++ shell/src/app/decl.d.ts
@@ -2,3 +2,4 @@ declare module 'mfe1/web-components';
 declare module 'mfe2/web-components';
 declare module 'mfe3/web-components';
 declare module 'mfe4/web-components';
+declare module 'mfe5/web-components';
